1e8a7ad59d4f62ad9efb1d12b947c21b6218b2d0
Karl Gordon <kgordon@stsci.edu>
2017-04-19 10:53:24 -0400

    More updates for getting all running online

--- pydirtygrid/__init__.py
+++ pydirtygrid/__init__.py
@@ -7,10 +7,10 @@ This is an Astropy affiliated package.
 # Affiliated packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
-#from ._astropy_init import *
-pass
+from ._astropy_init import *
 # ----------------------------------------------------------------------------
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-    from .example_mod import *
+    pass
+#    from .example_mod import *
