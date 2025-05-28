43a12d30e4efa3df2a186b748f6126d638facba8
Karl Gordon <kgordon@stsci.edu>
2025-05-28 15:10:33 -0400

    fixing filenames

--- pydirtygrid/rename_spec_files.py
+++ pydirtygrid/rename_spec_files.py
@@ -8,16 +8,17 @@ if __name__ == '__main__':
     filepath = "/astro/dust_kg3/klaw/cloudy2/nasa_fits"
     outpath = "/astro/dust_kg3/kgordon/DirtyGrid"
 
-    alldirs = glob.glob(f"{filepath}/??") + glob.glob(f"{filepath}/?")
+    alldirs = glob.glob(f"{filepath}/Z") + glob.glob(f"{filepath}/ZZZ?")
     mdirs = sorted(alldirs)
 
     for cmdir in mdirs:
         print(cmdir)
-        psdirs = glob.glob(f"{cmdir}/*")
+        psdirs = glob.glob(f"{cmdir}/19")
         for csdir in psdirs:
             pfiles = glob.glob(f"{csdir}/*.fits")
             for cfile in pfiles:
                 newname = f"s{cfile.split("-s")[1]}"
+                newname = cfile[cfile.find("-s"):]
 
                 # info from filename
                 ggeom = (cfile.split("-")[-1]).split("_")[0]
@@ -29,20 +30,19 @@ if __name__ == '__main__':
                 gtypes = [s for s in header["COMMENT"] if "/Models/" in s]
                 gtype = ((gtypes[0].split("/"))[-1]).lower()
 
-                ffactors = [s for s in header["COMMENT"] if "filling_factor=" in s]
+                ffactors = [s for s in header["COMMENT"] if "density_ratio=" in s]
                 ffactor = (ffactors[0].split("="))[-1]
-                if "0.15" in ffactors[0]:
+                if "0.01" in ffactors[0]:
                     lgeom = "clumpy"
+                    newname = newname.replace(f"-{ggeom}", "")
                 else:
                     lgeom = "homo"
+                    newname = newname.replace(f"-{lgeom}_{ggeom}", "")
 
-                newname = newname.replace(f"_{ggeom}", "")
-                newname = f"{ggeom}_{lgeom}_{gtype}_{newname}"
+                newname = f"{gtype}_{ggeom}_{lgeom}{newname}"
 
-                print(newname, cfile)
-
-                newpath = f"{outpath}/{ggeom}/{lgeom}/{gtype}"
-                print(newpath)
+                newpath = f"{outpath}/{gtype}/{ggeom}/{lgeom}"
+                print(f"{newpath}/{newname}", cfile)
                 if not os.path.exists(newpath):
                     os.makedirs(newpath)
 
